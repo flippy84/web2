0bf13e67bf31496527456bb2bd546f72343762fe
flippy84 <ante84@gmail.com>
2017-07-10 15:21:54 +0200

    Removed some unused parts

--- Server/Global.asax.cs
+++ Server/Global.asax.cs
@@ -17,7 +17,6 @@ namespace Server
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
