366e8bc576a9732e56375ce9face50aa5ec5eec1
flippy84 <ante84@gmail.com>
2017-07-11 09:07:23 +0200

    Started on api controller and model

---
+++ Server/Controllers/ApiController.cs
@@ -0,0 +1,56 @@
+﻿using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Server.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [ActionName("Task")]
+        public JsonResult GetTask(int ?id)
+        {
+            if(id != null)
+            {
+                return new ApiModel().GetTask(id.Value);
+            }
+
+            return Json(new { Error = "No id specified" }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpDelete]
+        [ActionName("Task")]
+        public JsonResult DeleteTask(int? id)
+        {
+            return Json(1);
+        }
+
+        [HttpGet]
+        [ActionName("Test")]
+        public JsonResult TestGet()
+        {
+            return Json("hej");
+        }
+
+        public ActionResult Test2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Task")]
+        public JsonResult PostTask(int? id)
+        {
+            return Json(1);
+        }
+    }
+}
---
+++ Server/Models/ApiModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace Server.Models
+{
+    public class ApiModel
+    {
+        public JsonResult GetTask(int id)
+        {
+            var db = new DatabaseModel();
+            var json = new JsonResult();
+            var task = db.Tasks
+                .Where(x => x.TaskID == id)
+                .Select(x => x);
+
+            if (task.Count() == 1)
+            {
+                json.Data = task;
+            }
+            else
+            {
+                json.Data = new { Error = "Couldn't find the specified task" };
+            }
+
+            json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return json;
+        }
+    }
+}
