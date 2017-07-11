071168296690f72d1be7880bb8c01355b9894611
flippy84 <ante84@gmail.com>
2017-07-11 16:53:32 +0200

    Server api works now

--- Server/Controllers/ApiController.cs
+++ Server/Controllers/ApiController.cs
@@ -10,47 +10,106 @@ namespace Server.Controllers
     public class ApiController : Controller
     {
         // GET: Api
-        public ActionResult Index()
+        public JsonResult Index()
         {
-            return View();
+            return JsonError("No default action");
         }
+
+        /*
+         * Task actions
+         */
 
         [HttpGet]
         [ActionName("Task")]
-        public JsonResult GetTask(int ?id)
+        public JsonResult GetTask(int? taskID)
         {
-            if(id != null)
+            var api = new ApiModel();
+
+            if(taskID.HasValue)
             {
-                return new ApiModel().GetTask(id.Value);
+                if (api.GetTask(taskID.Value) != null)
+                {
+                    return Json(api.GetTask(taskID.Value), JsonRequestBehavior.AllowGet);
+                }
+            }
+            else if(api.GetTasks() != null)
+            {
+                return Json(api.GetTasks(), JsonRequestBehavior.AllowGet);
             }
 
-            return Json(new { Error = "No id specified" }, JsonRequestBehavior.AllowGet);
+            return JsonError("Invalid request");
         }
 
-        [HttpDelete]
-        [ActionName("Task")]
-        public JsonResult DeleteTask(int? id)
-        {
-            return Json(1);
-        }
+        /*
+         * Assignment actions
+         */
 
         [HttpGet]
-        [ActionName("Test")]
-        public JsonResult TestGet()
+        [ActionName("Assignment")]
+        public JsonResult GetAssignment(int? taskID, int? userID)
         {
-            return Json("hej");
-        }
+            var api = new ApiModel();
+            if(taskID.HasValue && userID.HasValue)
+            {
+                if (api.GetAssignment(taskID.Value, userID.Value) != null)
+                {
+                    return Json(api.GetAssignment(taskID.Value, userID.Value), JsonRequestBehavior.AllowGet);
+                }
+            }
+            else if(taskID.HasValue)
+            {
+                if (api.GetAssignments(taskID.Value) != null)
+                {
+                    return Json(api.GetAssignments(taskID.Value), JsonRequestBehavior.AllowGet);
+                }
+            }
 
-        public ActionResult Test2()
-        {
-            return View();
+            return JsonError("Invalid request");
         }
 
         [HttpPost]
-        [ActionName("Task")]
-        public JsonResult PostTask(int? id)
+        [ActionName("Assignment")]
+        public JsonResult CreateAssignment(int? taskID, int? userID)
         {
-            return Json(1);
+            if (taskID != null && userID != null && new ApiModel().CreateAssignment(taskID.Value, userID.Value))
+            {
+                return JsonSuccess("Assignment created successfully");
+            }
+
+            return JsonError("Invalid request");
+        }
+
+        [HttpDelete]
+        [ActionName("Assignment")]
+        public JsonResult DeleteAssignment(int? taskID, int? userID)
+        {
+            if(taskID != null && userID != null)
+            {
+                return Json(new ApiModel().DeleteAssignment(taskID.Value, userID.Value));
+            }
+
+            return JsonError("Invalid request");
+        }
+
+        /*
+         * Helper methods for JSON responses
+         */
+
+        private JsonResult JsonError(string error)
+        {
+            return new JsonResult {
+                Data = new { Error = error },
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        private JsonResult JsonSuccess(string success)
+        {
+            return new JsonResult
+            {
+                Data = new { Success = success },
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
         }
     }
 }
--- Server/Models/ApiModel.cs
+++ Server/Models/ApiModel.cs
@@ -1,29 +1,99 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Server.Models
 {
     public class ApiModel
     {
-        public JsonResult GetTask(int id)
+        public Task GetTask(int taskID)
         {
             var db = new DatabaseModel();
-            var json = new JsonResult();
             var task = db.Tasks
-                .Where(x => x.TaskID == id)
+                .Where(x => x.TaskID == taskID)
                 .Select(x => x);
 
             if (task.Count() == 1)
             {
-                json.Data = task;
-            }
-            else
-            {
-                json.Data = new { Error = "Couldn't find the specified task" };
+                return task.Single();
             }
 
-            json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
-            return json;
+            return null;
+        }
+
+        public Task[] GetTasks()
+        {
+            var db = new DatabaseModel();
+            var tasks = db.Tasks
+                .Select(x => x);
+
+            if(tasks.Count() > 0)
+                return tasks.ToArray();
+
+            return null;
+        }
+
+        public Assignment GetAssignment(int taskID, int userID)
+        {
+            var db = new DatabaseModel();
+            var assignment = db.Assignments
+                .Where(x => x.TaskID == taskID && x.UserID == userID)
+                .Select(x => x)
+                .SingleOrDefault();
+
+            return assignment;
+        }
+
+        public Assignment[] GetAssignments(int taskID)
+        {
+            var db = new DatabaseModel();
+            var assignments = db.Assignments
+                .Where(x => x.TaskID == taskID)
+                .Select(x => x);
+
+            if(assignments.Count() > 0)
+                return assignments.ToArray();
+
+            return null;
+        }
+
+        public bool DeleteAssignment(int taskID, int userID)
+        {
+            var db = new DatabaseModel();
+            var assignment = db.Assignments
+                .Where(x => x.TaskID == taskID && x.UserID == userID)
+                .Select(x => x);
+
+            if (assignment.Count() == 1)
+            {
+                db.Assignments.DeleteOnSubmit(assignment.Single());
+                db.SubmitChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool CreateAssignment(int taskID, int userID)
+        {
+            var db = new DatabaseModel();
+            var assignment = new Assignment
+            {
+                TaskID = taskID,
+                UserID = userID
+            };
+
+            try
+            {
+                db.Assignments.InsertOnSubmit(assignment);
+                db.SubmitChanges();
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
