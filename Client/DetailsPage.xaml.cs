0d2cfb994b29691bc7a850fa1dc9f7094f05d448
flippy84 <ante84@gmail.com>
2017-07-16 22:08:41 +0200

    Added details page

---
+++ Client/DetailsPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Client
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DetailsPage : Page
+    {
+        public DetailsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if(e.Parameter is int)
+            {
+                var task = new ClientAPI("172.16.80.2").GetTask((int)e.Parameter);
+
+                Title.Text = task.Title;
+                Requirements.Text = task.Requirements;
+                Begin.Text = task.BeginDateTime.ToString();
+                Deadline.Text = task.DeadlineDateTime.ToString();
+
+                Assignments.Text = "";
+                foreach(var assignment in task.assignments)
+                {
+                    Assignments.Text += assignment.UserID.ToString() + "\n";
+                }
+            }
+            base.OnNavigatedTo(e);
+        }
+    }
+}
