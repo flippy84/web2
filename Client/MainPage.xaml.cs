986c3f5d509f0654e48d913e1f76899d14d6357a
flippy84 <ante84@gmail.com>
2017-07-18 18:35:10 +0200

    Colors and placement of buttons fixed

--- Client/MainPage.xaml.cs
+++ Client/MainPage.xaml.cs
@@ -17,6 +17,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Windows.UI;
 
 namespace Client
 {
@@ -38,24 +39,43 @@ namespace Client
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            int x = 110;
+            int y = 110;
             var tasks = api.GetTasks();
+            tasks.Sort((a, b) => (int)(a.BeginDateTime - b.BeginDateTime).TotalMilliseconds);
+            DateTime start = tasks[0].BeginDateTime;
+            DateTime end = tasks.Last().DeadlineDateTime;
+
             for (int i = 0; i < tasks.Count; i++)
             {
                 var task = tasks[i];
                 var button = new Button
                 {
                     Content = task.Title,
-                    Margin = new Thickness(10, x * i, 0, 0),
+                    Margin = new Thickness((task.BeginDateTime - start).TotalHours * 10 + 10, y * i, 0, 0),
                     Height = 50,
-                    Width = 200,
+                    Width = (task.DeadlineDateTime - task.BeginDateTime).TotalHours * 10,
                     Tag = task.TaskID,
+                    Name = task.TaskID.ToString()
                 };
+
+                if (task.assignments.Exists(x => x.UserID == CurrentUser.UserID) && task.assignments.Count == 1)
+                {
+                    button.Background = new SolidColorBrush(Colors.Green);
+                }
+                else if (task.assignments.Exists(x => x.UserID == CurrentUser.UserID) && task.assignments.Count > 1)
+                {
+                    button.Background = new SolidColorBrush(Colors.Red);
+                }
+                else if (task.assignments.Count > 0)
+                {
+                    button.Background = new SolidColorBrush(Colors.Blue);
+                }
 
                 button.Tapped += Button_Tapped;
                 button.Holding += Button_Holding;
 
                 MainContent.Children.Add(button);
+                MainContent.Width = (end - start).TotalHours * 10 + 20;
             }
         }
 
@@ -90,6 +110,17 @@ namespace Client
             var api = new ClientAPI("172.16.80.2");
             var taskID = (int)((MenuFlyoutItem)sender).Tag;
             api.CreateAssignment(taskID, CurrentUser.UserID);
+
+            Button button = (Button)FindName(taskID.ToString());
+            switch (api.GetTask(taskID).assignments.Count)
+            {
+                case 1:
+                    button.Background = new SolidColorBrush(Colors.Green);
+                    break;
+                default:
+                    button.Background = new SolidColorBrush(Colors.Red);
+                    break;
+            }
         }
 
         private void Disclaim_Tapped(object sender, TappedRoutedEventArgs e)
@@ -97,6 +128,17 @@ namespace Client
             var api = new ClientAPI("172.16.80.2");
             var taskID = (int)((MenuFlyoutItem)sender).Tag;
             api.DeleteAssignment(taskID, CurrentUser.UserID);
+
+            Button button = (Button)FindName(taskID.ToString());
+            switch (api.GetTask(taskID).assignments.Count)
+            {
+                case 0:
+                    button.Background = new SolidColorBrush(Colors.LightGray);
+                    break;
+                default:
+                    button.Background = new SolidColorBrush(Colors.Blue);
+                    break;
+            }
         }
 
         private void Button_Tapped(object sender, TappedRoutedEventArgs e)
